4198b14d3ed61c3afaaf204b6e54d1f5c3aceaf2
Rafael Gabriel <rafaeldzn2003@poli.ufrj.br>
2026-06-23 20:50:09 -0300

    [Feat] Book creation implementation

---
+++ pythonFunctions/createBook.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+def createBook(dbName, title, releaseDate, author, gender):
+    # Connect to the database
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+
+    # Create the book table if it doesnt exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            releaseDate TEXT NOT NULL,
+            author TEXT NOT NULL,
+            gender TEXT NOT NULL 
+        )
+    ''')
+
+    # Put the new user into the database
+    cursor.execute('''
+        INSERT INTO books (title, releaseDate, author, gender)
+        values (?, ?, ?, ?)
+    ''', (title, releaseDate, author, gender))
+
+    # Get the new id (last one generated)
+    bookId = cursor.lastrowid
+
+    # Save the changes and close the connection
+    conn.commit()
+    conn.close()
+
+    return bookId
