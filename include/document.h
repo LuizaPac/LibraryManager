f69f0838b924f58e142287ac37d630029b7cbbc8
Rafael Gabriel <rafael.gabriel@oceanpact.com>
2026-07-04 16:30:25 -0300

    [Feat] document.h header only implementation

--- include/document.h
+++ include/document.h
@@ -1,11 +1,51 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 namespace Library{
 
+class InvalidDocument : public std::exception {
+    public:
+        virtual const char *what() const throw(){
+            return "ERROR: Document should be in the format XXXXXXXXXXX.";
+        }
+};
+
 struct Document{
     std::string number;
+
+    Document(std::string documentNumber){
+        // Validation
+        if (documentNumber.length() != 11){
+            throw InvalidDocument();
+        }
+        this->number = documentNumber;
+    }
+
+    bool operator==(const Document &otherDocument) const{
+        return this->number == otherDocument.number;
+    }
+
+    friend std::ostream &operator<<(std::ostream &output, const Document &document){
+        output << document.number.substr(0, 3) << "." << document.number.substr(3, 3) << "." << document.number.substr(6, 3) << "-" << document.number.substr(9, 2) << std::endl;
+        return output;
+    }
+
+    friend std::istream &operator>>(std::istream &input, Document &document){
+        std::string tempNumber;
+
+        input >> tempNumber;
+
+        // Validation (same as constructor)
+        if (tempNumber.length() != 11){
+            throw InvalidDocument();
+        }
+
+        document.number = tempNumber;
+
+        return input;
+    }
 };
 
 }
